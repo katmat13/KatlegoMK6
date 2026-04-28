d843b634d3a0719448cd0c7d1eb028d1f546f67d
katmat13 <katlegomathabe@gmail.com>
2026-04-28 19:05:18 +0200

    Added retry to loginTest.js to handle rate limiting error

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -13,9 +13,16 @@ export const options = {
 
 export default function loginTest() {
     const response = loginRequest(PAYLOADS.login);
+    const body = response.json();
     console.log(`Response status: ${response.status}`);
     console.log(`Response body: ${response.body}`);
     validateLoginResponse(response);
     sleep (TEST_CONFIG.sleepTime, TEST_CONFIG.vus);
+
+
+    // Check for rate limit error and retry if necessary
+    if (body.error_code === 'RATE_LIMIT_EXCEEDED') {
+    console.warn('Rate limit hit, retrying after 5s...');
+    sleep(5);
    // return response;
-}
+}}
